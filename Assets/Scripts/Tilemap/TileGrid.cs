427f503674e9262a540806a201d121eb8eabab8c
Inanis lector <kid@kde.kiev.ua>
2023-06-16 22:38:26 +0300

    Tile position edit

--- Assets/Scripts/Tilemap/TileGrid.cs
+++ Assets/Scripts/Tilemap/TileGrid.cs
@@ -121,7 +121,7 @@ namespace TileMap
         {
             Vector2Int gridSpawnPosition = GetTileXY(spawnPosition);
 
-            spawnTile.globalPosition = GetTileWorldPos(gridSpawnPosition);
+            spawnTile.Position = GetTileWorldPos(gridSpawnPosition);
             tiles[gridSpawnPosition.x, gridSpawnPosition.y] = spawnTile;
 
             //Instantiate(spawnTile);
@@ -131,7 +131,7 @@ namespace TileMap
         {
             Vector2Int gridSpawnPosition = GetTileXY(spawnTile.gameObject.transform.position);
 
-            spawnTile.globalPosition = GetTileWorldPos(gridSpawnPosition);
+            spawnTile.Position = GetTileWorldPos(gridSpawnPosition);
             tiles[gridSpawnPosition.x, gridSpawnPosition.y] = spawnTile;
         }
     }
