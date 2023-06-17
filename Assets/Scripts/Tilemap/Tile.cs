1a4128edea4d45e5620dcf43293cf41cce0b1f11
Yarik <cahloyaroslav@gmail.com>
2023-06-17 09:13:26 +0300

    Added first ability to tile (heal by village`s neighbour tiles)

--- Assets/Scripts/Tilemap/Tile.cs
+++ Assets/Scripts/Tilemap/Tile.cs
@@ -1,11 +1,28 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
+public enum InfluenceNeighboursType
+{
+    Moor,
+    Neumann,
+}
 namespace TileMap
 {
     public class Tile : MonoBehaviour
     {
-        [field: SerializeField] public TileType type { get; private set; }
+        private TileType _type;
+        public TileType type
+        {
+            get => _type;
+            private set
+            {
+                _type = value;
+                TypeProcessing();
+            }
+        }
+        public InfluenceNeighboursType _influenceNeighboursType;
+
         //[SerializeField] private Structure _structure;
         [HideInInspector] public Vector2Int gridPosition;
         [SerializeField] private Player.Player player;
@@ -13,6 +30,8 @@ namespace TileMap
         private int damage;
         private int heal;
         public Action OnPlayerEnter;
+
+        private Tile[] tilesWithInfluence  = new Tile[0];
 
         public Vector2 Position
         {
@@ -27,6 +46,12 @@ namespace TileMap
         {
             TileGrid.instance.AddTile(this);
         }
+
+        private void Start()
+        {
+            TypeProcessing();
+        }
+
         public void SetDamage(int damage)
         {
             this.damage = damage;
@@ -43,6 +68,35 @@ namespace TileMap
         public void GiveHealToPlayer()
         {
             player.GetHeal(heal);
+        }
+
+        private void TypeProcessing()
+        {
+            foreach (Tile _tile in tilesWithInfluence)
+            {
+                _tile.OnPlayerEnter = null;
+            }
+            
+            switch (_influenceNeighboursType)
+            {
+                case InfluenceNeighboursType.Moor:
+                    tilesWithInfluence = TileGrid.instance.GetMooreNeighbourTiles(this);
+                    break;
+                case InfluenceNeighboursType.Neumann:
+                    tilesWithInfluence = TileGrid.instance.GetNeumannNeighbourTiles(this);
+                    break;
+            }
+            
+            switch (type)
+            {
+                case TileType.Village:
+                    foreach (var _tile in tilesWithInfluence)
+                    {
+                        _tile.SetHeal(5);
+                        _tile.OnPlayerEnter += _tile.GiveHealToPlayer;
+                    }
+                    break;
+            }
         }
     }
 }
