d20f62413f10426faee8fd29575812dd8b590c3a
Yarik <cahloyaroslav@gmail.com>
2023-06-17 20:17:41 +0300

    Working on fighting

--- Assets/Scripts/Fighting/Fighter.cs
+++ Assets/Scripts/Fighting/Fighter.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class Fighter : MonoBehaviour
 {
+    public Fighting fighting;
     public IDamagable damagable;
 
     [SerializeField] private Vector2 startPosition;
@@ -14,20 +15,21 @@ public class Fighter : MonoBehaviour
     public IDamagable target;
 
 
-    public IEnumerator AttackCoroutine()
+    public void AttackCoroutine()
     {
         AttackMove();
-        yield return new WaitForSeconds(2);
         target.Damage(damagable.damage);
     }
 
     private void AttackMove()
     {
-        transform.DOMoveX(transform.parent.position.x + enemyPosition.x, 1).SetEase(Ease.InElastic).onComplete = MoveBack;
+        transform.DOMoveX(transform.parent.position.x + enemyPosition.x, .12f).SetEase(Ease.InElastic).onComplete = MoveBack;
         Debug.Log("start pos " + startPosition.ToString());
         Debug.Log("enemy pos " + enemyPosition.ToString());
+        fighting.playerTurn = !fighting.playerTurn;
+
     }
 
     private void MoveBack()
-        => transform.DOMoveX(transform.parent.position.x + startPosition.x, 1).SetEase(Ease.InElastic);
+        => transform.DOMoveX(transform.parent.position.x + startPosition.x, .12f).SetEase(Ease.InElastic);
 }
